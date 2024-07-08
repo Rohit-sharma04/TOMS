ad02024dfcb46a1f5a145c5429df7a3df09462b1
rohit <rohitsharma4102002@gmail.com>
2024-07-08 18:12:18 +0530

    added more logs

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -4,7 +4,10 @@ export const authMiddleware = (req, res, next) => {
     try {
         // Extract the token from the cookies
         const token = req.cookies.token;
+        console.log("req",req)
+        console.log("req.cookies",req.cookies)
         console.log("token",token)
+        
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
