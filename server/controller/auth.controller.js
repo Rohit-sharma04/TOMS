e2a97e6e82f721c05c1708304230ebd9d3a012dd
rohit <rohitsharma4102002@gmail.com>
2024-07-09 22:09:54 +0530

    added and used public folder

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -82,10 +82,10 @@ export const loginController = async (req, res) => {
        
         // res.cookie('token', token, { httpOnly: false , sameSite: 'None'});
         res.cookie('token', token, {
-            httpOnly: true, 
+            httpOnly: false, 
             maxAge: maxAgeInMilliseconds,
             sameSite: 'None', // Important for cross-site cookies
-            secure: process.env.NODE_MODE === 'production' // Ensure cookies are only sent over HTTPS
+            secure: true||process.env.NODE_MODE === 'production' // Ensure cookies are only sent over HTTPS
         });
         
         res.status(200).json({ message: "Login successful",  success: true, role });
--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ import { configCloudinary } from "./config/cloudinary.js"
 import multer from "multer"
 import { handleStripeWebhookEvent } from "./controller/payment.controller.js"
 
+import path from 'path'
+
+
 //dotenv conig 
 dotenv.config()
 
@@ -32,7 +35,7 @@ app.use(cookieParser());
 // Configure CORS middleware with credentials:true to allow cookies in cross-origin requests
 app.use(cors({
     credentials: true,
-    origin: [process.env.CLIENT_URL,'https://toms-frontend.netlify.app','https://toms.onrender.com','  http://localhost:5173']
+    origin: [process.env.CLIENT_URL,'https://toms-frontend.netlify.app','https://toms.onrender.com','http://localhost:5173']
 }))
 
 app.post('/webhook', express.raw({ type: 'application/json' }), handleStripeWebhookEvent);
@@ -42,6 +45,16 @@ app.use("/api/officer", upload.any(), officerRoute);
 app.use("/api/admin", upload.any(), adminRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/stripe', paymentRoute);
+
+// Serve static files from the React app
+const __dirname = path.resolve(); // To resolve the current directory
+app.use(express.static(path.join(__dirname, 'public', 'dist')));
+
+// Catch-all handler to serve index.html for any other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'public', 'dist', 'index.html'));
+});
+
 
 //port 
 const port = process.env.PORT || 8080;
